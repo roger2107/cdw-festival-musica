2645701b3e93758d1ea1f8083004549d0df97b81
roger2107 <rogero.dev@gmail.com>
2023-05-12 23:18:20 -0500

    Variables principales creadas, HTML del header creado

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
-const {src, dest} = require('gulp');
+const {src, dest, watch} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const plumber =  require('gulp-plumber');
 
 function css(done){
-    src('src/scss/app.scss') //Identificar el archivo SASS
+    src('src/scss/**/*.scss') //Identificar el archivo SASS
+        .pipe(plumber())
         .pipe(sass()) //Compilarlo
         .pipe(dest('build/css'));  //Almacenarlo en el disco duro
 
     done(); //Indica cuando la funcion llega a su fin
 }
 
+
+function dev(done){
+    watch('src/scss/**/*.scss', css);
+
+    done();
+}
+
 exports.css = css;
+exports.dev = dev;
