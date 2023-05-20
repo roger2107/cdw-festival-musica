d93c5c685529caf0428d5d73a37fc0df0fa259c2
roger2107 <rogero.dev@gmail.com>
2023-05-19 20:29:55 -0500

    Codigo CSS y JS optimizado y minificado

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,13 @@ const {src, dest, watch, parallel} = require('gulp');
 //CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber =  require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+
+//JavaScript
+const terser = require('gulp-terser-js');
 
 //Imagenes
 const cache = require('gulp-cache');
@@ -12,8 +19,11 @@ const avif = require('gulp-avif');
 
 function css(done){
     src('src/scss/**/*.scss') //Identificar el archivo SASS
+        .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(sass()) //Compilarlo
+        .pipe(postcss([ autoprefixer(),cssnano() ]))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/css'));  //Almacenarlo en el disco duro
 
     done(); //Indica cuando la funcion llega a su fin
@@ -51,6 +61,9 @@ function versionAvif(done){
 
 function javascript(done){
     src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/js'));
 
     done();
